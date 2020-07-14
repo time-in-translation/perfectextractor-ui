81e66732784ddfb14a7fdaba053d1bb7cf0e3cc6
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2020-07-14 12:11:51 +0200

    Released 0.1.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setuptools.setup(
     name='perfectextractor-ui',
-    version='0.1.3',
+    version='0.1.4',
     author='Ben Bonfil',
     author_email='bonfil@gmail.com',
     description='A web frontend for perfectextractor',
